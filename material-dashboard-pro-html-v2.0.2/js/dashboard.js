c4bfd50e793093f23c4dc813026b005fe5deeab9
CLS <inspecpro18@gmail.com>
2018-09-15 19:24:58 +0800

    Manage User

--- material-dashboard-pro-html-v2.0.2/js/dashboard.js
+++ material-dashboard-pro-html-v2.0.2/js/dashboard.js
@@ -2,6 +2,22 @@ var db = firebase.firestore();
 
 $(document).ready(function(){
     loadDetails();
+
+    /*$("#Search").off('click').on('click', function(){
+        var value = $("#SearchText").val();
+        console.log(value);
+        $(".table tbody tr").filter(function() {
+            console.log($("#SearchText").text().toLowerCase().indexOf(value) > -1);
+            $("#SearchText").toggle($("#SearchText").text().toLowerCase().indexOf(value) > -1)
+          });
+    });*/
+
+    $("#SearchText").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $(".table tbody tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
 });
 
 function loadDetails()
@@ -10,18 +26,32 @@ function loadDetails()
     if (!user) {
         window.location = 'login.html';
         }
+        $('#userprofile').html(user.email);
+        
         var userId = user.uid;
 
         var userdocRef = db.collection("users").doc(userId);
 
         userdocRef.get().then(function(doc) {
             if (doc.exists) {
-                $('#userprofile').html(doc.data().email);
+               property(doc.data().property_id,doc.data().unit_id);
             }
             else {
                 // doc.data() will be undefined in this case
                 console.log("No such document!");
             }
+        });
+    });
+}
+
+function property(propertyID,unitID)
+{
+    var propertydocRef = db.collection("properties").doc(propertyID).collection("units").doc(unitID).collection("unit_members");
+
+    propertydocRef.get().then(function(querySnapshot){
+        querySnapshot.forEach(function(doc){
+            
+            $(".table tbody").append("<tr><td class='name'>"+doc.data().member_name+"</td><td></tr>");
         });
     });
 }
