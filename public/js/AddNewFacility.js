576d1bff909db607c6bbbc60d0abe88425f77272
CLS <inspecpro18@gmail.com>
2018-09-23 09:43:22 +0800

    storage

--- public/js/AddNewFacility.js
+++ public/js/AddNewFacility.js
@@ -1,11 +1,15 @@
 var db = firebase.firestore();
 var propertyID = sessionStorage.getItem("propertyID");
 var selected_file;
+var ref = firebase.storage().ref();
+var metadata = {
+    contentType: 'image/jpg'
+};
 
 $(document).ready(function(){
     loadDetails();
 
-    $("#Add").off('click').on('click', function(){
+    $("#file-submit").off('click').on('click', function(){
         AddNewFacility();
     });
 });
@@ -27,76 +31,37 @@ function AddNewFacility()
     // console.log(unitID);
     var name = $("#name").val();
     var description = $("#description").val();
-    // var fileButton = $("#fileButton");
 
-    // var blob = new Blob([selected_file], { type: "image/jpeg" });
-    // var filename = selected_file.name;
-    // var storageRef = firebase.storage().ref(filename);
-    // var uploadTask = storageRef.put(blob);
+    var propertydocRef = db.collection("properties").doc(propertyID).collection("facilities").doc();
 
-    // uploadTask.on('state_changed',
-    // function progress(snapshot){
-    //     var percentage = (snapshot.byteTransferred / snapshot.totalBytes) * 100;
-    //     uploader.value = percentage;
-    //     console.log(percentage)
-    // }, function error(error){
-
-    // }, function complete(){
-        
-    // });
-    // console.log(propertyID);
-    // console.log(selected_file);
-    // fileButton.addEventListener('change',function(e){
-    //     var file = e.target.files[0];
-
-    //     var storageRef = firebase.storage().ref(file.name);
-
-    //     var task = storageRef.put(file);
-
-    //     task.on('state_changed',
-
-    //         function progress(snapshot)
-    //         {
-
-    //         },
-    //         function error(err)
-    //         {
-
-    //         },
-    //         function complete()
-    //         {
-
-    //         }
-    //     )
-    // })
-    var blob = new Blob([selected_file], { type: "image/jpg" });
     var filename = selected_file.name;
-    var storageRef = firebase.storage().ref(filename);
-    var uploadTask = storageRef.put(blob);
+    //var storageRef = firebase.storage.ref(filename);
+    //var uploadTask = storageRef.put(selected_file);
+    var uploadTask = ref.child(filename).put(selected_file,metadata);
 
-    uploadTask.on('state_changed',function(snapshot){
-
-    },function(error){
-
-    },function(){
-        var downloadURL = uploadTask.snapshot.downloadURL;
-
-        var propertydocRef = db.collection("properties").doc(propertyID).collection("facilities");
-
-        propertydocRef.add({
-            facility_booking_enabled : true,
-            facility_description: description,
-            facility_id:"",
-            facility_image_url: downloadURL,
-            facility_time_description:"",
-            facility_title:name
-        })
-        .then(function() {
-            alert("The data has been saved successfully!");
-        })
-
+    uploadTask
+    .then(snapshot => snapshot.ref.getDownloadURL())
+    .then((url) => {
+        console.log(url)
     })
+//     uploadTask.on('state_changed',function(snapshot){
 
+//     }, function(error) {
+
+//     }, function() {
+//         var downloadURL = uploadTask.snapshot.downloadURL;
+//         propertydocRef.set({
+//                 facility_booking_enabled : true,
+//                 facility_description: description,
+//                 facility_id:propertydocRef.id,
+//                 facility_image_url: downloadURL,
+//                 facility_time_description:"",
+//                 facility_title:name
+//         }).then(function() {
+//             var ID = propertydocRef.id;
+//         })
+// });
+     
     
 }
 
