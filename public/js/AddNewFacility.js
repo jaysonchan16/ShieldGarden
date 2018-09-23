176a6e0fdeae7ec5222b7ad2c2783770498792bf
CLS <inspecpro18@gmail.com>
2018-09-23 17:27:12 +0800

    Upload images to storage as well as put the downloadurl to firstore

--- public/js/AddNewFacility.js
+++ public/js/AddNewFacility.js
@@ -1,15 +1,12 @@
 var db = firebase.firestore();
 var propertyID = sessionStorage.getItem("propertyID");
+var storage = firebase.storage();
 var selected_file;
-var ref = firebase.storage().ref();
-var metadata = {
-    contentType: 'image/jpg'
-};
 
 $(document).ready(function(){
     loadDetails();
 
-    $("#file-submit").off('click').on('click', function(){
+    $("#Add").off('click').on('click', function(){
         AddNewFacility();
     });
 });
@@ -35,32 +32,31 @@ function AddNewFacility()
     var propertydocRef = db.collection("properties").doc(propertyID).collection("facilities").doc();
 
     var filename = selected_file.name;
-    //var storageRef = firebase.storage.ref(filename);
-    //var uploadTask = storageRef.put(selected_file);
-    var uploadTask = ref.child(filename).put(selected_file,metadata);
+    var storageRef = firebase.storage().ref(filename);
+    var uploadTask = storageRef.put(selected_file);
+    uploadTask.on('state_changed',
+    
+    function(snapshot){
 
-    uploadTask
-    .then(snapshot => snapshot.ref.getDownloadURL())
-    .then((url) => {
-        console.log(url)
-    })
-//     uploadTask.on('state_changed',function(snapshot){
+    }, function(error) {
 
-//     }, function(error) {
-
-//     }, function() {
-//         var downloadURL = uploadTask.snapshot.downloadURL;
-//         propertydocRef.set({
-//                 facility_booking_enabled : true,
-//                 facility_description: description,
-//                 facility_id:propertydocRef.id,
-//                 facility_image_url: downloadURL,
-//                 facility_time_description:"",
-//                 facility_title:name
-//         }).then(function() {
-//             var ID = propertydocRef.id;
-//         })
-// });
+    }, function() {
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+            console.log('File available at', downloadURL);
+          
+            propertydocRef.set({
+                    facility_booking_enabled : true,
+                    facility_description: description,
+                    facility_id:propertydocRef.id,
+                    facility_image_url: downloadURL,
+                    facility_time_description:"",
+                    facility_title:name
+            }).then(function() {
+                var ID = propertydocRef.id;
+                console.log(ID);
+            })
+    });
+});
      
     
 }
@@ -70,12 +66,14 @@ function readURL(input) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            selected_file = e.target.result;
+            
             $('#blah')
                 .attr('src', e.target.result)
                 .height(200);
         };
 
+        selected_file = input.files[0];
+      //  console.log(selected_file);
         reader.readAsDataURL(input.files[0]);
     }
 }
