2661f0ee52fcf360dbe4f375c89a1fe054403703
CLS <inspecpro18@gmail.com>
2018-09-29 12:30:39 +0800

    Add New User adding field

--- public/js/AddNewUser.js
+++ public/js/AddNewUser.js
@@ -16,13 +16,14 @@ $(document).ready(function(){
 
 function loadDetails()
 {
-    firebase.auth().onAuthStateChanged(function(user) {
+     firebase.auth().onAuthStateChanged(function(user) {
+    var user = firebase.auth().currentUser;
     if (!user) {
         window.location = 'login.html';
         }
         $('#userprofile').html(user.email);
 
-    });
+     });
 }
 
 function addnewuser()
@@ -32,17 +33,62 @@ function addnewuser()
     var memberName = $("#newusername").val();
     var memberEmail = $("#newemail").val();
     var memberContactNumber = '+6' + $("#newcontact").val();
+    var memberPassword = $("#newpassword").val();
 
     var propertydocRef = db.collection("properties").doc(propertyID).collection("units").doc(unitID).collection("unit_members");
+    
+    firebase.auth().createUserWithEmailAndPassword(memberEmail, memberPassword)
+    .then(function(firebaseUser){
+        var uid = firebaseUser.user.uid;
+        
+        var property = $("#assignedproperty option:selected").val();
+        var unit = $("#assignedunit option:selected").val();
+        var fullAddress = unit +", "+property;
 
-    propertydocRef.add({
-        member_name: memberName,
-        member_email: memberEmail,
-        member_ContactNumber:memberContactNumber,
-        member_property:propertyName,
-        member_unit:unitID
+        var userdocRef = db.collection("users").doc(uid);
+        
+        userdocRef.set({
+            admin: null,
+            app_platform: null,
+            app_version: null,
+            country_code: null,
+            email:memberEmail,
+            full_address:fullAddress,
+            name:memberName,
+            phone_model:null,
+            phone_number:memberContactNumber,
+            property_id:propertyID,
+            unit_id:unitID,
+            user_id:uid
+        }).then(function()
+        {
+            propertydocRef.add({
+                member_name: memberName,
+                member_password:memberPassword,
+                member_email: memberEmail,
+                member_ContactNumber:memberContactNumber,
+                member_property:property,
+                member_unit:unit,
+                member_id:uid
+            })
+            .then(function() {
+                alert("The data has been saved successfully!");
+                firebase.auth().signOut();
+            })
+            .catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+            });             
+        })
+        .catch(function(error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
     })
-    .then(function() {
-        alert("The data has been saved successfully!");
-    })
+    .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    });
+
+    
 }
